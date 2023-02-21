33528657e36cdb5faaeb35f70c2427d5756bdbac
Yassine Bayed <Yassine.bayed12@gmail.com>
2023-02-21 11:56:18 -0800

    prints _putchar followed by new line

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * Description :
+ * ifndef : check if the main.h is already defined or not
+ * in case the ifndef is TRUE the define is skipped
+ *
+ * the declarations below represnt the prototype of functions
+ */
+int _putchar(char c);
+#endif
